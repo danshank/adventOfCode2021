6cb6311ac98c1305994a539c25844ef6f05ccbfa
Daniel Shank <dan.shank13@gmail.com>
2021-12-03 07:02:29 -0500

    Finish Day 2

---
+++ src/main/scala/Day2.scala
@@ -0,0 +1,51 @@
+import scala.io.Source
+
+enum Movement:
+    case Forward(n: Int)
+    case Up(n: Int)
+    case Down(n: Int)
+
+def solveDayTwoPartOne(): Int =
+    val filename = "src/main/scala/Day2.txt"
+    try
+        val rawInput = Source.fromFile(filename).getLines()
+        val directions = rawInput.map(parseDirection)
+        val initialCoordinate = (0, 0)
+        val (x, y) = directions.foldLeft(initialCoordinate)(followDirection)
+        x * y
+    catch case e: Exception =>
+        println(s"Exception occurred: $e")
+        0
+
+def solveDayTwoPartTwo(): Int =
+    val filename = "src/main/scala/Day2.txt"
+    try
+        val rawInput = Source.fromFile(filename).getLines()
+        val directions = rawInput.map(parseDirection)
+        val initialCoordinate = (0, 0, 0)
+        val (x, y, _) = directions.foldLeft(initialCoordinate)(followDirectionAndAim)
+        x * y
+    catch case e: Exception =>
+        println(s"Exception occurred: $e")
+        0    
+
+def parseDirection(s: String): Movement =
+    val directionComponents = s.split("\\s+")
+    directionComponents(0) match
+        case "forward" => Movement.Forward(directionComponents(1).toInt)
+        case "up" => Movement.Up(directionComponents(1).toInt)
+        case "down" => Movement.Down(directionComponents(1).toInt)
+    
+def followDirection(position: (Int, Int), direction: Movement): (Int, Int) =
+    val (x, y) = position
+    direction match
+        case Movement.Forward(n) => (x + n, y)
+        case Movement.Up(n) => (x, y - n)
+        case Movement.Down(n) => (x, y + n)
+    
+def followDirectionAndAim(position: (Int, Int, Int), direction: Movement): (Int, Int, Int) =
+    val (x, y, aim) = position
+    direction match
+        case Movement.Forward(n) => (x + n, y + (aim * n), aim)
+        case Movement.Up(n) => (x, y, aim - n)
+        case Movement.Down(n) => (x, y, aim + n)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
 @main def solveAdventDay(day: Int, part: Int): Unit = 
   if day == 1 && part == 1 then println(solveDayOnePartOne())
   else if day == 1 && part == 2 then println(solveDayOnePartTwo())
+  else if day == 2 && part == 1 then println(solveDayTwoPartOne())
+  else if day == 2 && part == 2 then println(solveDayTwoPartTwo())
